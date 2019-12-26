30683820eb64b5c187e139cefc1710f5d70ddc82
Erik Pehrers <erik.pehrers@jetshop.se>
2019-12-26 22:18:52 +0100

    I think this little helper made it a bit easier

--- src/reducers/memoryReducer.ts
+++ src/reducers/memoryReducer.ts
@@ -1,12 +1,14 @@
 import { MemoryState, Action } from '../context/memory-context'
 import { isLength } from '../utils/isLength'
 
+const isEqual = (itemOne: string | number, itemTwo: string | number) => itemOne === itemTwo
+
 // In order to have number of cards as a setting, 
 // the reducer payload must be of type MemoryCard.
 export function memoryReducer(state: MemoryState, action: Action): MemoryState {
     switch (action.type) {
-        case 'SELECT':
 
+        case 'SELECT':
             const { card } = action.payload.selectedCard
             const { cards, selectedCards } = state
 
@@ -16,17 +18,17 @@ export function memoryReducer(state: MemoryState, action: Action): MemoryState {
             }
 
             // Prevent from selecting the same card over and over.
-            if (isLength(selectedCards, 1) && card.uniqueId === selectedCards.find(c => c.uniqueId === card.uniqueId)?.uniqueId) {
+            if (isLength(selectedCards, 1) && card.uniqueId === selectedCards.find(c => isEqual(c.uniqueId, card.uniqueId))?.uniqueId) {
                 return state;
             }
 
             if (isLength(selectedCards, 2)) {
                 const [c1, c2] = selectedCards;
 
-                if (c1.memoryId === c2.memoryId && c1.uniqueId !== c2.uniqueId) {
+                if (isEqual(c1.memoryId, c2.memoryId) && !isEqual(c1.uniqueId, c2.uniqueId)) {
                     // Match!
                     const collectedCards = cards.map(c => {
-                        if (c1.uniqueId === c.uniqueId || c2.uniqueId === c.uniqueId) {
+                        if (isEqual(c1.uniqueId, c.uniqueId) || isEqual(c2.uniqueId, c.uniqueId)) {
                             c.isCollected = true
                         }
                         return c;
@@ -51,7 +53,7 @@ export function memoryReducer(state: MemoryState, action: Action): MemoryState {
 
             // Always open at least one card.
             const openCard = cards.map(c => {
-                if (c.uniqueId === card.uniqueId) {
+                if (isEqual(c.uniqueId, card.uniqueId)) {
                     c.isOpen = true;
                 }
                 return c;
