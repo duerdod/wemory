e58de198294840495ce614be58dc10e5e5cf6a73
Erik Pehrers <erik.pehrers@jetshop.se>
2020-05-26 18:15:10 +0200

    Group actions, add some comments

--- src/machine/memoryMachine.ts
+++ src/machine/memoryMachine.ts
@@ -27,37 +27,65 @@ const isNotSelected = (context: MemoryState, event: AnyEventObject) => {
     return openCards.find((c: MemoryCard) => c.uniqueId === event.card.uniqueId) ? false : true;
 }
 
-const resetGame = assign<MemoryState>({
-    ...initialState,
-    cards: (_, event: AnyEventObject) => {
-        const { cardCount, cardType } = event.gameSettings
-        return generateCards(cardCount, cardType)
-    },
-    cardType: (_, event: AnyEventObject) => event.gameSettings.cardType,
-})
+const actions = {
+    resetGame: assign<MemoryState>({
+        ...initialState,
+        cards: (_, event: AnyEventObject) => {
+            const { cardCount, cardType } = event.gameSettings
+            return generateCards(cardCount, cardType)
+        },
+        cardType: (_, event: AnyEventObject) => event.gameSettings.cardType,
+    }),
 
-const flipAllButCollected = assign<MemoryState>({
-    selectedCards: () => [],
-    cards: (context) => context.cards.map((card: MemoryCard) => ({ ...card, isOpen: card.isCollected }))
-})
+    flipAllButCollected: assign<MemoryState>({
+        selectedCards: () => [],
+        cards: (context) => context.cards.map((card: MemoryCard) => ({ ...card, isOpen: card.isCollected }))
+    }),
 
-const selectCard = assign<MemoryState>({
-    cards: (context, event: AnyEventObject) => {
-        const { card } = event
-        const { cards } = context
+    selectCard: assign<MemoryState>({
+        cards: (context, event: AnyEventObject) => {
+            const { card } = event
+            const { cards } = context
 
-        const openCard = cards.map((c: MemoryCard) => ({
-            ...c,
-            isOpen: isEqual(c.uniqueId, card.uniqueId)
-                ? true
-                : c.isOpen
-        }))
+            const openCard = cards.map((c: MemoryCard) => ({
+                ...c,
+                isOpen: isEqual(c.uniqueId, card.uniqueId)
+                    ? true
+                    : c.isOpen
+            }))
 
-        return openCard
-    },
-    selectedCards: (context: MemoryState, event: AnyEventObject) => [...context.selectedCards, event.card],
-    moves: (context: MemoryState) => context.moves + 1,
-})
+            return openCard
+        },
+        selectedCards: (context: MemoryState, event: AnyEventObject) => [...context.selectedCards, event.card],
+        moves: (context: MemoryState) => context.moves + 1,
+    }),
+
+    checkMatch: assign<MemoryState>({
+        cards: (context) => {
+            const { cards, selectedCards }: { cards: MemoryCard[], selectedCards: MemoryCard[] } = context;
+
+            // Could this be a guard instead?
+            if (hasLength(selectedCards, 2)) {
+                const [c1, c2] = selectedCards;
+
+                if (isMatch(c1, c2)) {
+                    const collectedCards = cards.map(c => ({
+                        ...c,
+                        isOpen: false,
+                        isCollected: isEqual(c1.uniqueId, c.uniqueId) || isEqual(c2.uniqueId, c.uniqueId)
+                            ? true
+                            : c.isCollected
+                    }))
+
+                    return collectedCards
+
+                }
+            }
+            // else
+            return cards
+        }
+    })
+}
 
 const checkMatch = assign<MemoryState>({
     cards: (context) => {
@@ -106,6 +134,8 @@ export const MemoryMachine = Machine<MemoryState, MemorySchema, MemoryEvents>({
     states: {
         idle: {
             on: {
+                // Mostly here if I ever extend the machine.
+                // Idle is also a great state to be in if for example the settings modal is open.
                 '': 'playing'
             }
         },
@@ -138,6 +168,9 @@ export const MemoryMachine = Machine<MemoryState, MemorySchema, MemoryEvents>({
                 },
                 checking: {
                     onEntry: 'checkMatch',
+                    // Maybe add a shortcut to 'first' if there is a match. 
+                    // Otherwise you'll have to wait until the timer below is finished, even if it's a match. 
+                    // Which is kind of frustrating.
                     after: {
                         700: {
                             target: '#Memory.playing',
@@ -148,7 +181,6 @@ export const MemoryMachine = Machine<MemoryState, MemorySchema, MemoryEvents>({
             }
         },
         gameWon: {
-            onEntry: 'setGameWon',
             on: {
                 RESET: {
                     target: 'playing',
@@ -165,10 +197,7 @@ export const MemoryMachine = Machine<MemoryState, MemorySchema, MemoryEvents>({
     }
 }, {
     actions: {
-        selectCard,
-        flipAllButCollected,
-        checkMatch,
-        resetGame
+        ...actions
     },
     guards: {
         isGameWon,
